b0d4603b5ce8fa7ac1c36a8a29e1a49f3e6c2452
Andreas Mahnke <andreas.mahnke@leuphana.de>
2025-10-20 15:41:34 +0200

    tests

--- backend/test/controller/item_controller_test.rb
+++ backend/test/controller/item_controller_test.rb
@@ -5,27 +5,41 @@ module Inventory
     require 'test/unit'
     require 'mocha/test_unit'
 
+
     class ItemControllerTest < Test::Unit::TestCase
-      def test_add_item_with_barcode
 
-        data = {}
-        barcode_cache = {}
+      def create_barcode_cache_mock
+        mock_barcode_cache = mock('barcode_cache')
 
+        Inventory::Storage::BarcodeCacheStorage
+          .stubs(:new)
+          .returns(mock_barcode_cache)
+
+        mock_barcode_cache
+      end
+
+      def create_storage_mock
         mock_storage = mock('storage')
-        mock_storage.expects(:load_data).returns(data)
-        mock_storage.expects(:store_data).returns(true)
 
         Inventory::Storage::DataStorage
           .stubs(:new)
           .returns(mock_storage)
 
-        mock_barcode_cache = mock('barcode_cache')
+        mock_storage
+      end
+      
+      def test_add_item_with_barcode
+
+        data = {}
+        barcode_cache = {}
+
+        mock_storage = create_storage_mock
+        mock_storage.expects(:load_data).returns(data)
+        mock_storage.expects(:store_data).returns(true)
+
+        mock_barcode_cache = create_barcode_cache_mock
         mock_barcode_cache.expects(:load_data).returns(barcode_cache)
         mock_barcode_cache.expects(:store_data).returns(true)
-
-        Inventory::Storage::BarcodeCacheStorage
-          .stubs(:new)
-          .returns(mock_barcode_cache)
 
         item_controller = ItemController.new
         assert_same(false, data.key?('1'))
@@ -44,20 +58,12 @@ module Inventory
           }
         }
 
-        mock_storage = mock('storage')
+        mock_storage = create_storage_mock
         mock_storage.expects(:load_data).returns(data).twice
         mock_storage.expects(:store_data).returns(true).twice
 
-        Inventory::Storage::DataStorage
-          .stubs(:new)
-          .returns(mock_storage)
-
-        mock_barcode_cache = mock('barcode_cache')
+        mock_barcode_cache = create_barcode_cache_mock
         mock_barcode_cache.expects(:load_data).returns(barcode_cache).twice
-
-        Inventory::Storage::BarcodeCacheStorage
-          .stubs(:new)
-          .returns(mock_barcode_cache)
 
         item_controller = ItemController.new
         assert_same(2, data['1']['4251097410289'])
